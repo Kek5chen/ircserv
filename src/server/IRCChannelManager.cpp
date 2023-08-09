a2363472d19bcdebddb95cbf3890a643134fba46
Kek5chen <kx@imkx.dev>
2023-08-09 15:39:40 +0200

    idfk. name or something

--- src/server/IRCChannelManager.cpp
+++ src/server/IRCChannelManager.cpp
@@ -1,9 +1,12 @@
 #include "server/IRCChannelManager.hpp"
 
 IRCChannel* IRCChannelManager::get(const std::string& channelName) {
-	if (m_channels.find(channelName) == m_channels.end())
+	std::string rawName = channelName;
+	if (rawName[0] == '#')
+		rawName = rawName.substr(1);
+	if (m_channels.find(rawName) == m_channels.end())
 		return 0;
-	return m_channels[channelName];
+	return m_channels[rawName];
 }
 
 IRCChannel* IRCChannelManager::get_or_create(const std::string& channelName, IRCClient* requester) {
