0c6517e39c6a88be5e258324f67ef5647163c07a
Kek5chen <kx@imkx.dev>
2023-08-11 19:04:43 +0200

    fix: removed unnecessary space from PING response

--- src/server/IRCServer.cpp
+++ src/server/IRCServer.cpp
@@ -230,7 +230,7 @@ void IRCServer::handleUSER(IRCClient *client, const IRCCommand &cmd) {
 }
 
 void IRCServer::handlePING(IRCClient *client, const IRCCommand &cmd) {
-	const std::string &add = cmd.mParams.empty() ? "" : " " + cmd.mParams[0];
+	const std::string &add = cmd.mParams.empty() ? "" : cmd.mParams[0];
 	IRCServer::getResponseBase().setCommand("PONG")
 		.setEnd(add)
 		.sendTo(client);
