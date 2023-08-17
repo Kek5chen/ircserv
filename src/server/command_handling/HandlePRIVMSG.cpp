fb48ca0930bd300918a90aa01d232f45e07cc6db
kx <kx@imkx.dev>
2023-08-17 11:52:24 +0200

    fix: add param 1 to message

--- src/server/command_handling/HandlePRIVMSG.cpp
+++ src/server/command_handling/HandlePRIVMSG.cpp
@@ -2,7 +2,7 @@
 
 void IRCServer::handlePRIVMSG(IRCClient *client, const IRCCommand &cmd) {
 	const std::string &channel = cmd.mParams[0];
-	const std::string &message = cmd.mEnd;
+	const std::string &message = (cmd.mParams.size() > 1 ? cmd.mParams[1] : "") + cmd.mEnd;
 	mChannelManager.send(client, channel.substr(1),
 						 client->getResponseBase().setCommand("PRIVMSG")
 								 .addParam(channel)
