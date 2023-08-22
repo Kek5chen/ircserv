6771562e33f2ef4a591122d015686c84dc256e3d
Kek5chen <kx@imkx.dev>
2023-08-22 08:39:49 +0200

    fix: signals

--- src/setup/SignalHandlers.cpp
+++ src/setup/SignalHandlers.cpp
@@ -1,28 +1,36 @@
 #include "setup/SignalHandlers.hpp"
-#include "server/IRCServer.hpp"
 #include "utils/nullptr.hpp"
+#include "server/IRCServer.hpp"
+#include "utils/Logger.hpp"
 #include <csignal>
 #include <cstdlib>
-
-NullType nilptr = {};
+#include <stdexcept>
+#include <iostream>
 
 static IRCServer *g_ctx;
 
 void graceful_signal_handler(int signal) {
 	(void) signal;
+	LOG("ZO");
 	g_ctx->stop();
 	exit(0);
 }
 
 void registerSignals(IRCServer *ctx) {
 	g_ctx = ctx;
-	std::signal(SIGABRT, graceful_signal_handler);
-	std::signal(SIGTERM, graceful_signal_handler);
-	std::signal(SIGHUP, graceful_signal_handler);
+	if (std::signal(SIGABRT, graceful_signal_handler) == SIG_ERR)
+		throw std::runtime_error("Could not register SIGABRT handler");
+	if (std::signal(SIGTERM, graceful_signal_handler) == SIG_ERR)
+		throw std::runtime_error("Could not register SIGTERM handler");
+	if (std::signal(SIGHUP, graceful_signal_handler) == SIG_ERR)
+		throw std::runtime_error("Could not register SIGPIPE handler");
+	if (std::signal(SIGINT, graceful_signal_handler) == SIG_ERR)
+		throw std::runtime_error("Could not register SIGPIPE handler");
 }
 
 void unregisterSignals() {
 	g_ctx = nilptr;
 	std::signal(SIGABRT, SIG_DFL);
 	std::signal(SIGTERM, SIG_DFL);
+	std::signal(SIGHUP, SIG_DFL);
 }
