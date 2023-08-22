ff3dd77dad9276d0b8649bd14f08dda70cb02113
Kek5chen <kx@imkx.dev>
2023-08-22 09:59:51 +0200

    fix: cancel when trying to reregister

--- src/server/command_handling/HandlePASS.cpp
+++ src/server/command_handling/HandlePASS.cpp
@@ -2,11 +2,13 @@
 #include "server/CodeDefines.hpp"
 
 bool IRCServer::handlePASS(IRCClient *client, const IRCCommand &cmd) {
-	if (client->hasAccess() && !client->mSuppliedPassword.empty())
+	if (client->hasAccess() && !client->mSuppliedPassword.empty()) {
 		IRCServer::getResponseBase().setCommand(ERR_ALREADYREGISTRED)
 			.addParam(client->getNickname())
 			.setEnd("You may not reregister")
 			.sendTo(client);
+		return true;
+	}
 	client->mSuppliedPassword = cmd.mParams[0];
 	bool hasAccess = client->hasAccess();
 	if (!hasAccess)
