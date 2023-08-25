26b792ab2e74e0673b90e2e150055ad42d2ecee5
Kek5chen <kx@imkx.dev>
2023-08-25 10:49:56 +0200

    fix: catch any exception in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,31 @@
 #include "utils/Logger.hpp"
 
 int main(int argc, const char **argv) {
-	InitData initData(argc, argv);
-	if (!initData.isValid()) {
-		LOG(RED("Could not initialize client.\n") << RED(initData.getError()));
-		return 0;
+	int serverExitStatus = 0;
+	try {
+		InitData initData(argc, argv);
+		if (!initData.isValid()) {
+			LOG(RED("Could not initialize client.\n") << RED(initData.getError()));
+			return 0;
+		}
+
+		LOG(YELLOW("Starting IRC Server..."));
+		LOG("- Port: " << initData.getPort());
+		LOG("- Password: " << initData.getPassword());
+		IRCServer server(initData.getPort(), initData.getPassword(), initData.getIp());
+		registerSignals(&server);
+		server.bind();
+		LOG(GREEN("Server socket bound"));
+
+		server.listen();
+		LOG("Server listening on port " << initData.getPort());
+
+		LOG(MAGENTA("--- Start of Server Logs ---"));
+
+		serverExitStatus = server.loop();
+	} catch (const std::exception &e) {
+		LOG(RED("An error occurred: ") << RED(e.what()));
 	}
-
-	LOG(YELLOW("Starting IRC Server..."));
-	LOG("- Port: " << initData.getPort());
-	LOG("- Password: " << initData.getPassword());
-	IRCServer server(initData.getPort(), initData.getPassword(), initData.getIp());
-	registerSignals(&server);
-	server.bind();
-	LOG(GREEN("Server socket bound"));
-
-	server.listen();
-	LOG("Server listening on port " << initData.getPort());
-
-	LOG(MAGENTA("--- Start of Server Logs ---"));
-
-	int serverExitStatus = server.loop();
-
 	LOG("Server exited with status " << serverExitStatus);
 	LOG(MAGENTA("--- End of Server Logs ---"));
 
